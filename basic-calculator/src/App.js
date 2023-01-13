67a6c7147eba5544cd06d11a1b915db0bc4d18e7
Andrea Godoshian <94014358+andreagodoshian@users.noreply.github.com>
2023-01-13 16:44:46 -0500

    day four
    "I have three production outages named after me... four." -Programmers Are Also Human

--- basic-calculator/src/App.js
+++ basic-calculator/src/App.js
@@ -1,42 +1,36 @@
-// r-r-r-REMIX! Guess it's time for React.js
-// Thank you: https://www.youtube.com/watch?v=DgRrrOt0Vr8&t=9s
-// "I have three production outages named after me... four."
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button.js";
 
-import { useReducer } from "react";
-import "./styles.css";
-
-// function reducer(state, action) {}
-function reducer(state, action) {
-
-}
-
-function App() {
-  // const [state, dispatch] = useReducer(reducer, {})
-  const [ {currentOperand, previousOperand, operation}, dispatch ] = useReducer(reducer, {})
+const App = () => {
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+    <div className="App">
+      <div className="calc-wrapper">
+        <div className="row">
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+        </div>
+        <div className="row">
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+        </div>
+        <div className="row">
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+        </div>
+        <div className="row">
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+          <Button></Button>
+        </div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>×</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
     </div>
   )
 }
---
+++ basic-calculator/src/components/Button.js
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+const Button = () => {
+    return (
+        <div className="button-wrapper">HEJ, KVINNA!</div>
+    )
+}
+
+export default Button;
