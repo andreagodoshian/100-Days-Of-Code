b1700470447511e9190b6dd6f2cb6224b193f56d
Andrea Godoshian <94014358+andreagodoshian@users.noreply.github.com>
2023-01-15 21:13:19 -0500

    day six
    "Add Ingredients" works, but limited functionality

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -4,11 +4,13 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [ inputState, setInputState ] = useState({title: "", amount: ""});
+  // const [ inputState, setInputState ] = useState({title: "", amount: ""});
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount});
   };
 
   return (
@@ -20,12 +22,9 @@ const IngredientForm = React.memo(props => {
             <input 
             type="text" 
             id="title" 
-            value={inputState.title} 
+            value={enteredTitle} 
             onChange={event => {
-              const newTitle = event.target.value;
-              setInputState(prevInputState => ({
-                title: newTitle, amount: prevInputState.amount
-              }))
+              setEnteredTitle(event.target.value);
             }}/>
           </div>
           <div className="form-control">
@@ -33,12 +32,9 @@ const IngredientForm = React.memo(props => {
             <input 
             type="number" 
             id="amount" 
-            value={inputState.amount} 
+            value={enteredAmount} 
             onChange={event => {
-              const newAmount = event.target.value;
-              setInputState(prevInputState => ({
-                amount: newAmount, title: prevInputState.title
-              }))
+              setEnteredAmount(event.target.value);
             }}/>
           </div>
           <div className="ingredient-form__actions">
