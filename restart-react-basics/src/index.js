5de134f6d876153d98058d1cb08bb42c32339a39
Andrea Godoshian <94014358+andreagodoshian@users.noreply.github.com>
2023-01-19 15:18:24 -0500

    day ten

--- restart-react-basics/src/index.js
+++ restart-react-basics/src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom/client';
+// **remember: this is the first to execute
+////////////////////////////////////////////
 
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+// ^^css import doesn't work in vanilla javascript
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+// ^^this html also doesn't work in vanilla javascript
