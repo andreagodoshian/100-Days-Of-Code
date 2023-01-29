5dd580fb1f134faf2131c592ef7903da93660300
Andrea Godoshian <94014358+andreagodoshian@users.noreply.github.com>
2023-01-28 23:32:22 -0500

    day 19

---
+++ _2023_01_10_/src/main/java/org/example/day11to20/TwoSum.java
@@ -0,0 +1,35 @@
+package org.example.day11to20;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{2, 7, 11, 15};
+
+        int[] ans = solution(arr, 9);
+
+        for (int x: ans) {
+            System.out.println(x);
+        }
+
+    }
+
+    public static int[] solution(int[] nums, int target) {
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[1];
+            if (map.containsKey(diff)) {
+                ans[0] = i;
+                ans[1] = map.get(diff);
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return ans;
+    }
+
+}
