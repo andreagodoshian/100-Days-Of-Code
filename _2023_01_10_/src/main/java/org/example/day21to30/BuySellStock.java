53b76e56b8a0959f9153328d1568792caf2813ec
Andrea Selberg-Godoshian <kvinna138@gmail.com>
2023-02-04 21:26:01 -0500

    day 26

--- _2023_01_10_/src/main/java/org/example/day21to30/BuySellStock.java
+++ _2023_01_10_/src/main/java/org/example/day21to30/BuySellStock.java
@@ -1,4 +1,27 @@
 package org.example.day21to30;
 
 public class BuySellStock {
+
+    public static void main(String[] args) {
+        int[] myArr = new int[]{7, 1, 5, 3, 6, 4};
+
+        System.out.println(solution(myArr));
+    }
+
+    public static int solution(int[] prices) {
+
+        if (prices.length <= 1) return 0; // escape
+
+        int profit = 0;
+        int leftIndex = 0;
+
+        for (int rightIndex = 1; rightIndex < prices.length; rightIndex++) {
+            if (prices[rightIndex] > prices[leftIndex]) {
+                profit = Math.max(profit, (prices[rightIndex] - prices[leftIndex]));
+            } else {
+                leftIndex = rightIndex;
+            }
+        }
+        return profit;
+    }
 }
