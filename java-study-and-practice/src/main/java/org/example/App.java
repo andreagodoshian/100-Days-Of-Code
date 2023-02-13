70ceae0033d7b021580f0017c9e723a6f57285f9
Andrea <kvinna138@gmail.com>
2023-02-12 21:33:56 -0500

    day idk

--- java-study-and-practice/src/main/java/org/example/App.java
+++ java-study-and-practice/src/main/java/org/example/App.java
@@ -13,8 +13,25 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println("Hej Världen!");
+
+        System.out.println(solution(2));
+        System.out.println(solution(-2));
+        System.out.println(solution(20));
+        System.out.println(solution(202));
     }
 
-    public static void solution() {
+
+    public static boolean solution(int x) {
+        if (x<0) return false;
+        if(x<10) return true;
+        int remainder = 0;
+        int answer = 0;
+        int temp = x;
+        while (temp > 0) {
+            remainder = temp % 10;
+            answer = (answer * 10) + remainder;
+            temp /= 10;
+        }
+        return (answer == x);
     }
 }
