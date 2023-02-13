7ea300ccf6a1cd3f9c6668f63efbfc4f45d53044
Andrea <kvinna138@gmail.com>
2023-02-13 15:08:23 -0500

    It's alive 🧟‍♀️😵‍💫

---
+++ openapi-demo/src/main/java/com/freyaselberg/openapidemo/OpenapiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.freyaselberg.openapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// localhost:8080/swagger-ui.html
+@SpringBootApplication
+public class OpenapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiDemoApplication.class, args);
+	}
+
+}
