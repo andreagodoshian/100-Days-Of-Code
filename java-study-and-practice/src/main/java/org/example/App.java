60327a918f96c64de2ba77b5d87010376e389599
Andrea <kvinna138@gmail.com>
2023-02-15 16:16:53 -0500

    day 37
    Dang that was easier than I expected (MySQL && (DBeaver && Docker))

--- java-study-and-practice/src/main/java/org/example/App.java
+++ java-study-and-practice/src/main/java/org/example/App.java
@@ -3,8 +3,11 @@ package org.example;
 import java.util.*;
 
 /*
+RECURRING THEME(S): try to go through each collection once.
+As in, don't spend time converting a data structure BEFORE working with it (if that makes sense)
+
 NEED TO KNOW:
--Two Sum: 2-13 (gradually store in a hashmap, and look for the difference)
+-Two Sum
 -Valid Parenthesis
 -Palindrome checker
 -Common prefix
@@ -12,6 +15,7 @@ NEED TO KNOW:
 -Plus One Array: 2-13 (go backwards - address best case, then the worst case)
 -Remove Array Element
 -Remove Array Duplicates (track the insert index [starts at 1])
+-Search Insert
 */
 public class App
 {
@@ -19,8 +23,36 @@ public class App
 
         System.out.println("Hej! I shouldn't have eaten all those rice krispies :( not treats, just the cereal.");
 
+        int[] arr = new int[]{2, 7, 11, 15};
+        int[] ans1 = solution(arr, 9); // 0, 1
+        for (int x: ans1) {
+            System.out.print(x + " ");
+        }
+        int[] ans2 = solution(arr, 17); // 0, 3
+        for (int x: ans2) {
+            System.out.print(x + " ");
+        }
+
     }
 
-    public static void solution() {
+    public static int[] solution(int[] nums, int target) {
+
+        // Think. What do I need to solve this? value and the index... so HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // Look as the return: an array;
+        int[] answer = new int[2];
+
+        // remember - with these types of loops, you arguably have "index" built-in
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                answer[0] = i;
+                answer[1] = map.get(diff);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return answer;
     }
+
 }
--- java-study-and-practice/src/main/java/org/example/day31to40/SearchInsert.java
+++ java-study-and-practice/src/main/java/org/example/day31to40/SearchInsert.java
@@ -3,31 +3,33 @@ package org.example.day31to40;
 public class SearchInsert {
 
     public static void main(String[] args) {
+        int[] myArr = new int[]{1, 3, 5, 6};
+        System.out.println("Using array:");
+        for (int x:myArr) System.out.print(x + " ");
+        System.out.println();
 
+        System.out.println(solution(myArr, 5)); // answer: 2
+        System.out.println(solution(myArr, 2)); // answer: 1
+        System.out.println(solution(myArr, 7)); // answer: 4
     }
 
-    public static int searchInsert(int[] nums, int target) {
-
-        if (nums.length==0) return 0;
-
-        if (nums.length==1) {
-            if (target>nums[0]){
-                return 1;
-            } else return 0;
-        }
-
+    // search the list, and find out where to insert the target
+    public static int solution(int[] nums, int target) {
+        System.out.println("Target: " + target);
         int answer = 0;
-
-        for(int i=0;i<nums.length-1;i++){
-            if (target == nums[i]){
-                answer =  i;
-                break;
-            } else if (target>nums[i] && target<=nums[i+1]){
-                answer = i+1;
-                break;
-            } else if (target>nums[i+1] && (nums.length-1==i+1)){
-                answer= nums.length;
-                break;
+        if (nums.length == 0) return answer;
+        else {
+            for (int i = 0; i < nums.length; i++) {
+                if (target > nums[i] && target <= nums[i+1]) {
+                    answer = i + 1;
+                    break;
+                }
+                // need to catch BEFORE error (i.e. nums[i+1])
+                // general "else" won't work here
+                else if (target > nums[i+1] && nums.length-1 == i+1) {
+                    answer = nums.length;
+                    break;
+                }
             }
         }
         return answer;
